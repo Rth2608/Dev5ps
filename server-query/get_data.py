0d153c7e0b87d1764720495c4263bb1d3758a37f
sadam <sadam@sogang.ac.kr>
2025-05-09 12:20:08 +0900

    refactor: get_ohlcv(_data) only handle until data, read_ohlcv will handle Response

--- server-query/get_data.py
+++ server-query/get_data.py
@@ -1,38 +1,30 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import text
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from shared.connect_db import engine
-from shared.symbols_intervals import SYMBOLS, INTERVALS
 
 # symbol_interval 테이블에서 OHLCV 데이터를 조회하고 반환
-def get_ohlcv(symbol: str, interval: str):
+def get_ohlcv_data(symbol: str, interval: str) -> list[dict | None]:
 
-    if symbol not in SYMBOLS or interval not in INTERVALS:
-        return JSONResponse(content={"error": "테이블이 존재하지 않습니다."}, status_code=400)
-
-    table_name = f"{symbol.lower()}_{interval}"
-    query = text(f"SELECT * FROM {table_name} ORDER BY timestamp")
+    TABLE_NAME = f"{symbol.lower()}_{interval}"
+    # Query to select OHLCV
+    query = text(
+        'SELECT "timestamp", "open", "high", "low", "close", "volume" '\
+        f"FROM {TABLE_NAME} ORDER BY timestamp"
+        )
 
     try:
         with engine.connect() as conn:
             df = pd.read_sql(query, conn)
     except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        print(f"{TABLE_NAME} DB error: {e}")
+        raise e
 
     if df.empty:
-        return JSONResponse(content=[], status_code=200)
+        return []
 
-    # 필요한 데이터만 추출
-    columns_to_keep = ["timestamp", "open", "high", "low", "close", "volume"]
-    df = df[columns_to_keep]
-
-    # NaN, inf, -inf 제거 처리
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    # inf, -inf 제거 처리
+    df.replace([np.inf, -np.inf], np.nan, inplace = True)
     df = df.where(pd.notnull(df), None)
-    df = df.astype(object)
 
-    # DataFrame을 리스트 딕셔너리로 변환 후 FastAPI가 인식할 수 있도록 jsonable_encoder로 변환 후 JSON 응답 반환
-    safe_data = jsonable_encoder(df.to_dict(orient="records"))
-    return JSONResponse(content=safe_data)
+    return df.to_dict(orient = "records")
--- server-query/main_query.py
+++ server-query/main_query.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
-from get_data import get_ohlcv
+from fastapi.encoders import jsonable_encoder
+from get_data import get_ohlcv_data
 from shared.symbols_intervals import SYMBOLS, INTERVALS
 
 app = FastAPI()
@@ -16,8 +17,25 @@ app.add_middleware(
 
 # 캔들 데이터 호출 api
 @app.get("/ohlcv/{symbol}/{interval}")
-def read_ohlcv(symbol: str, interval: str):
-    data = get_ohlcv(symbol, interval)
-    if data is None:
-        raise HTTPException(status_code=404, detail="db조회 실패")
-    return data
+def read_ohlcv(
+        symbol: str,
+        interval: str,
+        ):
+    
+    symbol = symbol.upper()
+    interval = interval.lower()
+    if symbol not in SYMBOLS or interval not in INTERVALS:
+        raise HTTPException(
+            status_code = 400,
+            detail = "Invalid symbol or interval"
+            )
+    try:
+        data = get_ohlcv_data(symbol, interval)
+        safe_data = jsonable_encoder(data)
+        return safe_data
+    except Exception as e:
+        print(f"Error fetching data for {symbol}_{interval}: {e}")
+        raise HTTPException(
+            status_code = 500,
+            detail = "Internal Server Error"
+            )
